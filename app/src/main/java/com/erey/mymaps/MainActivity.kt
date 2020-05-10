76853d12db57e7dbcb3e48f71db09e3afa5ed928
Esteban Rey <erey@stanford.edu>
2020-05-10 05:09:23 -0700

    Completed Part 3

--- app/src/main/java/com/erey/mymaps/MainActivity.kt
+++ app/src/main/java/com/erey/mymaps/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.erey.mymaps
 
+import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.erey.mymaps.models.MapsAdapter
 import com.erey.mymaps.models.Place
 import com.erey.mymaps.models.UserMap
 import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_USER_MAP = "EXTRA_USER_MAP"
+private const val TAG= "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +21,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val userMaps = generateSampleData()
         rvMaps.layoutManager = LinearLayoutManager(this)
-        rvMaps.adapter = MapsAdapter(this, userMaps)
+        rvMaps.adapter = MapsAdapter(this, userMaps, object:MapsAdapter.OnClickLsitener{
+            override fun onItemClick(position: Int) {
+                Log.i("IDK","onItemClick $position")
+                val intent = Intent (this@MainActivity, DisplayMapActivity::class.java)
+                intent.putExtra(EXTRA_USER_MAP, userMaps[position])
+                startActivity(intent)
+            }
+
+        } )
 
     }
 
--- app/src/main/java/com/erey/mymaps/models/MapsAdapter.kt
+++ app/src/main/java/com/erey/mymaps/models/MapsAdapter.kt
@@ -7,7 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MapsAdapter (val context: Context, val userMaps: List<UserMap>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MapsAdapter (val context: Context, val userMaps: List<UserMap>, val onClickLsitener: OnClickLsitener): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    interface OnClickLsitener {
+        fun onItemClick(position: Int)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
         return ViewHolder(view)
@@ -17,6 +21,9 @@ class MapsAdapter (val context: Context, val userMaps: List<UserMap>): RecyclerV
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val userMap = userMaps[position]
+        holder.itemView.setOnClickListener({
+            onClickLsitener.onItemClick(position)
+        })
         val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
         textViewTitle.text = userMap.title
     }
