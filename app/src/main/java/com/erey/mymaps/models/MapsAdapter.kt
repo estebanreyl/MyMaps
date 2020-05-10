a891ac26648c39ac86549944a70458257f4c5dfa
Esteban Rey <erey@stanford.edu>
2020-05-10 03:59:36 -0700

    first part completed

---
+++ app/src/main/java/com/erey/mymaps/models/MapsAdapter.kt
@@ -0,0 +1,24 @@
+package com.erey.mymaps.models
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MapsAdapter (val context: Context, val userMaps: List<UserMap>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
---
+++ app/src/main/java/com/erey/mymaps/models/UserMap.kt
@@ -0,0 +1,3 @@
+package com.erey.mymaps.models
+
+data class UserMap(val title: String, val places: List<Place> )
